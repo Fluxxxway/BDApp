b509cf5e7d29cac48c72db597124ac53724244a9
Fluxxxway <goverbid@gmail.com>
2026-04-26 23:46:33 +0700

    New classse were added to project

--- BDApp/Data/ApplicationDbContext.cs
+++ BDApp/Data/ApplicationDbContext.cs
@@ -6,4 +6,32 @@ namespace BDApp.Data
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
     }
+
+    public class ConstructionProject
+    {
+        //public DbSet<BDApp.Models.ConstructionProject> Projects { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class ConstructionWorker
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public int ProjectID { get; set; }
+        public ConstructionProject Project { get; set; }
+    }
+
+    public class Material
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int ProjectID { get; set; }
+        public ConstructionProject Project { get; set; }
+    }
 }
