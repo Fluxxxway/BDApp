4923ddc52eccc7614fbf82f57fe3dfebdab781e1
Fluxxxway <goverbid@gmail.com>
2026-05-02 10:52:09 +0700

    added DB, Component, ComponentFile, Project
    Reworked Programm.cs. appsettings.json, ApplicationDBContext

---
+++ BDApp/Models/ComponentFile.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace BDApp.Models
+{
+    public class ComponentFile
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid ComponentId { get; set; }
+
+        [ForeignKey("ComponentId")]
+        public Component? Component { get; set; }
+
+        public int? DocumentTypeID { get; set; }
+        
+        [ForeignKey("DocumentTypeID")]
+        public DocumentType? DocumentType { get; set; } 
+
+        public string FileName { get; set; } = string.Empty;
+
+        public string FilePath { get; set; } = string.Empty;
+
+        public string? FileExtension { get; set; }
+
+        public long FileSize { get; set; }
+
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        public string? UploadedBy { get; set; }
+
+        public string? Notes { get; set; }
+    }
+
+    // Конкретный тип из 567 документов
+    public class DocumentType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public DocumentCategory Category { get; set; } = null!;
+
+        public string Name { get; set; } = null!; // "Акт освидетельствования скрытых работ"
+
+        public string? Code { get; set; } // Код по классификатору
+
+        public string? Description { get; set; }
+    }
+
+
+    public class DocumentCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; } = null!;
+
+        [StringLength(500)]
+        public string? Description { get; set; }
+
+        public List<DocumentType> DocumentTypes { get; set; } = new();
+    }
+
+}
---
+++ BDApp/Models/Project.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BDApp.Models
+{
+    public class Project
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(1000)]
+        public string? Description { get; set; }
+
+        [Required]
+        public string InviteCode { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public ICollection<Component> Components { get; set; } = new List<Component>();
+    }
+}
