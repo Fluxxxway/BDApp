6984661a4442300318420d6c7ee1ec190da20303
Fluxxxway <goverbid@gmail.com>
2026-05-17 21:44:18 +0700

    Resolving few conflicts in BDApp.csproj
    Some stings were merged into desktop UI
    Changes were made in BDApp.csproj
    Changes were made in Project.cs

--- BDApp/Models/Project.cs
+++ BDApp/Models/Project.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
 
 namespace BDApp.Models
 {
@@ -14,6 +15,7 @@ namespace BDApp.Models
 
         [Required]
         public string InviteCode { get; set; }
+        public string InviteCodeHash { get; set; } = string.Empty;
 
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
