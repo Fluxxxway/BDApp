1c71403849401f33d03c81b790f6a48d755023ca
Fluxxxway <goverbid@gmail.com>
2026-05-18 13:23:38 +0700

    Cleared DBCOntext
    i've removed stirng strings

--- BDApp/Data/ApplicationDbContext.cs
+++ BDApp/Data/ApplicationDbContext.cs
@@ -6,11 +6,6 @@ namespace BDApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public DbSet<ConstructionProject> Projects { get; set; }
-        public DbSet<ConstructionWorker> Workers { get; set; }
-        public DbSet<Material> Materials { get; set; }
-    }
-
         public DbSet<Project> Projects => Set<Project>();
         public DbSet<Component> Components => Set<Component>();
         public DbSet<ComponentFile> ComponentFiles => Set<ComponentFile>();
