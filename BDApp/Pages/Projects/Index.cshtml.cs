81f37467c7603c4eb7f8a75b015c6d3adac8a2e3
Fluxxxway <goverbid@gmail.com>
2026-05-26 11:27:25 +0700

    Refactor namespace and class name comments
    Updated namespace and class name comments for clarity.

--- BDApp/Pages/Projects/Index.cshtml.cs
+++ BDApp/Pages/Projects/Index.cshtml.cs
@@ -5,10 +5,10 @@ using BDApp.Data;
 using BDApp.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BDApp.Pages.Projects  // 🔥 Должно совпадать с папкой!
+namespace BDApp.Pages.Projects
 {
     [Authorize]
-    public class IndexModel : PageModel  // 🔥 Класс называется IndexModel!
+    public class IndexModel : PageModel
     {
         private readonly ApplicationDbContext _context;
 
